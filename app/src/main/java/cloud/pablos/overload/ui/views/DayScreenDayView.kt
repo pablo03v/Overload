a4601e1d545437a1ba5c3bd29025f44ce9d6ff5b
pabloscloud <93644977+pabloscloud@users.noreply.github.com>
2024-04-01 19:21:45 +0200

    - adds swipe between days functionality

---
+++ app/src/main/java/cloud/pablos/overload/ui/views/DayScreenDayView.kt
@@ -0,0 +1,239 @@
+package cloud.pablos.overload.ui.views
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import cloud.pablos.overload.R
+import cloud.pablos.overload.data.item.Item
+import cloud.pablos.overload.data.item.ItemEvent
+import cloud.pablos.overload.data.item.ItemState
+import cloud.pablos.overload.ui.tabs.configurations.OlSharedPreferences
+import cloud.pablos.overload.ui.tabs.home.HomeTabDeletePauseDialog
+import cloud.pablos.overload.ui.tabs.home.HomeTabEditItemDialog
+import java.time.LocalDate
+
+@SuppressLint("UnusedTransitionTargetStateParameter")
+@OptIn(ExperimentalFoundationApi::class)
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun DayScreenDayView(
+    daysCount: Int,
+    page: Int,
+    state: ItemState,
+    onEvent: (ItemEvent) -> Unit,
+    isEditable: Boolean,
+) {
+    val date =
+        LocalDate.now()
+            .minusDays((daysCount - page - 1).toLong())
+
+    val items =
+        state.items.filter { item ->
+            val startTime = parseToLocalDateTime(item.startTime)
+            extractDate(startTime) == date
+        }
+
+    val itemsDesc = items.sortedByDescending { it.startTime }
+
+    val deletePauseDialogState = remember { mutableStateOf(false) }
+    val editItemDialogState = remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val sharedPreferences = remember { OlSharedPreferences(context) }
+
+    val goalWork by remember { mutableIntStateOf(sharedPreferences.getWorkGoal()) }
+    val goalPause by remember { mutableIntStateOf(sharedPreferences.getPauseGoal()) }
+
+    if (itemsDesc.isNotEmpty()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            item {
+                Row(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(top = 10.dp, start = 10.dp, end = 10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
+                    if (goalWork > 0) {
+                        Box(
+                            modifier = Modifier.weight(1f),
+                        ) {
+                            DayViewProgress(goal = goalWork, items = items, isPause = false)
+                        }
+                    }
+
+                    if (goalPause > 0) {
+                        Box(
+                            modifier = Modifier.weight(1f),
+                        ) {
+                            DayViewProgress(
+                                goal = goalPause,
+                                items = items,
+                                date = date,
+                                isPause = true,
+                            )
+                        }
+                    }
+                }
+            }
+            val itemSize = itemsDesc.size
+            if (
+                items.isNotEmpty() &&
+                items.last().ongoing.not() &&
+                items.last().pause.not() &&
+                date == LocalDate.now()
+            ) {
+                item {
+                    Box(
+                        modifier =
+                            Modifier
+                                .padding(10.dp, 10.dp, 10.dp)
+                                .combinedClickable(
+                                    enabled = isEditable,
+                                    onLongClick = {
+                                        deletePauseDialogState.value = true
+                                    },
+                                    onClick = {
+                                        if (state.isDeletingHome) {
+                                            deletePauseDialogState.value = true
+                                        } else {
+                                            onEvent(ItemEvent.SetSelectedItemsHome(emptyList()))
+                                            editItemDialogState.value = true
+                                        }
+                                    },
+                                ),
+                    ) {
+                        DayViewItemOngoing(
+                            item =
+                                Item(
+                                    id = -1,
+                                    startTime = items.last().endTime,
+                                    endTime = LocalDate.now().toString(),
+                                    ongoing = true,
+                                    pause = true,
+                                ),
+                            isSelected = false,
+                            state = state,
+                        )
+                    }
+                }
+            }
+
+            items(itemSize) { index ->
+                val isFirstItem = index == 0
+                val isLastItem = index == itemSize - 1
+
+                val item = itemsDesc[index]
+                val isSelected = state.selectedItemsHome.contains(item)
+                Box(
+                    modifier =
+                        Modifier
+                            .padding(
+                                10.dp,
+                                if (isFirstItem) 10.dp else 0.dp,
+                                10.dp,
+                                if (isLastItem) 80.dp else 10.dp,
+                            )
+                            .combinedClickable(
+                                enabled = isEditable,
+                                onLongClick = {
+                                    onEvent(ItemEvent.SetIsDeletingHome(true))
+                                    onEvent(ItemEvent.SetSelectedItemsHome(listOf(item)))
+                                },
+                                onClick = {
+                                    if (state.isDeletingHome) {
+                                        when (isSelected) {
+                                            true ->
+                                                onEvent(
+                                                    ItemEvent.SetSelectedItemsHome(state.selectedItemsHome.filterNot { it == item }),
+                                                )
+
+                                            else ->
+                                                onEvent(
+                                                    ItemEvent.SetSelectedItemsHome(
+                                                        state.selectedItemsHome +
+                                                            listOf(
+                                                                item,
+                                                            ),
+                                                    ),
+                                                )
+                                        }
+                                    } else {
+                                        onEvent(ItemEvent.SetSelectedItemsHome(listOf(item)))
+                                        editItemDialogState.value = true
+                                    }
+                                },
+                            ),
+                ) {
+                    when (item.ongoing.not() && item.endTime.isNotBlank()) {
+                        true -> DayViewItemNotOngoing(item, isSelected = isSelected, state)
+                        else -> DayViewItemOngoing(item, isSelected = isSelected, state = state)
+                    }
+                }
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextView(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = stringResource(id = R.string.no_items_title),
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                fontWeight = FontWeight.Bold,
+                align = TextAlign.Center,
+                color = MaterialTheme.colorScheme.primary,
+            )
+            TextView(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = stringResource(id = R.string.no_items_subtitle),
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                fontWeight = FontWeight.Medium,
+                align = TextAlign.Center,
+                color = MaterialTheme.colorScheme.primary,
+            )
+        }
+    }
+
+    if (deletePauseDialogState.value) {
+        HomeTabDeletePauseDialog(onClose = { deletePauseDialogState.value = false })
+    }
+
+    if (editItemDialogState.value) {
+        HomeTabEditItemDialog(
+            onClose = {
+                onEvent(ItemEvent.SetSelectedItemsHome(emptyList()))
+                editItemDialogState.value = false
+            },
+            state,
+            onEvent,
+        )
+    }
+}
